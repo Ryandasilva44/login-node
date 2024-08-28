958bc7ac22cbdb62345a8efc9cb7a3b473df8cb2
ryan da silva <ryaneyesgreen@hotmail.com>
2024-08-27 23:46:54 -0300

    novo commit do node

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 // const bodyParser = require("body-parser");
 const cors = require("cors");
+const corsOptions = {
+  origin: 'http://localhost:4200', // ou o domínio do seu frontend
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 const users = [
   { name: "John Doe", email: "john@exemplo.com" },
