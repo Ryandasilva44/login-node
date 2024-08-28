52ac78a480e70aade10ce51be1ee44b51bd1f14b
ryan da silva <ryaneyesgreen@hotmail.com>
2024-08-27 23:18:31 -0300

    mas um commit novo

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 const users = [
